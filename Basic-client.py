6cb313a0d4026390b308450b30fb23badc3eceab
unknown <shynhsiri@gmail.com>
2026-05-09 13:59:24 -0700

    first

---
+++ Basic-client.py
@@ -0,0 +1,21 @@
+import socket
+
+target_host = "www.google.com"
+target_port = 80
+
+#socket object
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+#connect client
+client.connect((targrt_host,target_port))
+
+#send data
+client.send(b"GET / HTTP/1.1\r\nHost: google.com\r\n\r\n")
+
+#recive data
+response = client.recv(4096)
+
+print(response.decode())
+client.close()
+
+#COPY FROM BLACK HAT PYTHON BOOK
